94caa506c233bc49d1f98280e2b0ace7bc75b9d7
Vinícius César Bertolo Calligaris <vinicius.cesar7@hotmail.com>
2025-05-01 11:29:25 -0300

    Finalizado

--- calculadora_sincrona.v
+++ calculadora_sincrona.v
@@ -1,9 +1,34 @@
 module calculadora_sincrona(
+    input clk,
+    input rst,
     input [7:0] entrada,
     input [2:0] codigo,
     output reg [7:0] saida
 );
-    
-//insira seu código aqui
+    reg [7:0] acumulador;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            acumulador <= 8'b0;
+            saida <= 8'b0;
+        end else begin
+            case (codigo)
+                3'b000: saida <= entrada;
+
+                3'b001: begin
+                    acumulador <= acumulador + entrada;
+                    saida <= 8'b0;
+                end
+                3'b010: begin 
+                    acumulador <= acumulador - entrada;
+                    saida <= 8'b0;
+                end
+                
+                3'b011: saida <= acumulador;
+             
+                default: saida <= 8'b0;
+            endcase
+        end
+    end
 
 endmodule
